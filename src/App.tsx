5bf28ff0311551669ccc8feb7530f92fa8598ebb
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-19 17:40:08 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      Sedans Choose a sedan for its affordability and excellent fuel economy.
+      Ideal for cruising in the city or on your next road trip. SUVs Take an SUV
+      for its spacious interior, power, and versatility. Perfect for your next
+      family vacation and off-road adventures. Luxury Cruise in the best car
+      brands without the bloated prices. Enjoy the enhanced comfort of a luxury
+      rental and arrive in style.
+      <div className="attribution">
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
+          rel="noreferrer"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="/">Your Name Here</a>.
+      </div>
     </div>
   );
 }
